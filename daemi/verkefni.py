bb1a2459c7f9d56c89856f15c279216a2dbe4ac6
hrannarau <hrannarau239@nemi.tskoli.is>
2017-01-24 13:37:59 +0000

    Daemi 1

---
+++ daemi/verkefni.py
@@ -0,0 +1,11 @@
+#Git Verkefni   Dags. 24.1.2017
+#Hrannar Helgi Auðunsson
+
+#Dæmi 1
+print("Dæmi 1")
+tala1 = int(input("Sláðu inn eina tölu "))
+tala2 = int(input("Sláðu inn aðra tölu "))
+margf = tala1*tala2
+lagdar = tala1+tala2
+print("Tölurnar lagðar saman:",lagdar)
+print("Tölurnar margfaldaðar saman:",margf)
