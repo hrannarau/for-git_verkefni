18d1733243908e93230f94ba536db5d13e04c737
hrannarau <hrannarau239@nemi.tskoli.is>
2017-01-24 14:02:40 +0000

    Daemi 3

--- daemi/verkefni.py
+++ daemi/verkefni.py
@@ -15,3 +15,18 @@ print("Dæmi 2")
 fornafn = input("Sláðu inn fornafnið þitt ")
 eftirnafn = input("Sláðu inn eftirnafnið þitt ")
 print("Halló",fornafn,eftirnafn)
+
+#Dæmi 3
+print("Dæmi 3")
+texti = input("Sláðu inn einn streng ")
+hastafir = 0
+lagstafir = 0
+lagHa = 0
+for i in range(len(texti)):
+    if texti[i].isupper():
+        hastafir = hastafir+1
+        if texti[i + 1].islower():
+            lagHa = lagHa + 1
+    elif texti[i].islower():
+        lagstafir = lagstafir+1
+print(hastafir,"hástafir,",lagstafir,"lágstafir,",lagHa,"lágstafir koma beint eftir hástaf")
